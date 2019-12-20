e79a43c7ce3f8cf653dac11b4c2159d6ea30ea13
Slamet <slametngeblog@gmail.com>
2019-12-20 23:27:08 +0700

    Finish Alarm

--- app/src/main/java/id/slametriyadi/intentimplicit_imastudio/AlarmActivity.java
+++ app/src/main/java/id/slametriyadi/intentimplicit_imastudio/AlarmActivity.java
@@ -1,14 +1,70 @@
 package id.slametriyadi.intentimplicit_imastudio;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AnalogClock;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AlarmActivity extends AppCompatActivity {
+
+    @BindView(R.id.analog_clock)
+    AnalogClock analogClock;
+    @BindView(R.id.btn_set_alarm)
+    Button btnSetAlarm;
+    @BindView(R.id.tv_set_alarm)
+    TextView tvSetAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        ButterKnife.bind(this);
+    }
+
+    private TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+            Calendar calNow = Calendar.getInstance();
+            Calendar calSet = (Calendar) calNow.clone();
+
+            calSet.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            calSet.set(Calendar.MINUTE, minute);
+
+            if (calSet.compareTo(calNow) <= 0) {
+                calSet.add(Calendar.DATE, 1);
+            } else if (calSet.compareTo(calNow) > 0) {
+                Toast.makeText(AlarmActivity.this, "Alarm diset untuk " + calSet.getTime(), Toast.LENGTH_SHORT).show();
+            }
+
+            tvSetAlarm.setText("Alarm Berbunyi pada : " + calSet.getTime());
+
+            Intent setIntentAlarm = new Intent(getBaseContext(), AlarmReceiver.class);
+            PendingIntent pendingIntentAlarm = PendingIntent.getBroadcast(getBaseContext(), 1, setIntentAlarm, 0);
+
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            alarmManager.set(AlarmManager.RTC_WAKEUP, calSet.getTimeInMillis(), pendingIntentAlarm);
+        }
+    };
+
+
+    @OnClick(R.id.btn_set_alarm)
+    public void onViewClicked() {
+        Calendar calendar = Calendar.getInstance();
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE),true);
+        timePickerDialog.show();
     }
 }
---
+++ app/src/main/java/id/slametriyadi/intentimplicit_imastudio/AlarmReceiver.java
@@ -0,0 +1,17 @@
+package id.slametriyadi.intentimplicit_imastudio;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm berbunyi", Toast.LENGTH_SHORT).show();
+        MediaPlayer player = MediaPlayer.create(context, R.raw.alarm);
+        player.start();
+    }
+}
