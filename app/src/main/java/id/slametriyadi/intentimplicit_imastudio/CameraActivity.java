ef68f5de496ee1153a7a352d03105d73a8b122be
Slamet <slametngeblog@gmail.com>
2019-12-20 23:11:12 +0700

    Starter Project

---
+++ app/src/main/java/id/slametriyadi/intentimplicit_imastudio/CameraActivity.java
@@ -0,0 +1,14 @@
+package id.slametriyadi.intentimplicit_imastudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
---
+++ app/src/main/java/id/slametriyadi/intentimplicit_imastudio/WifiActivity.java
@@ -0,0 +1,14 @@
+package id.slametriyadi.intentimplicit_imastudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WifiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wifi);
+    }
+}
